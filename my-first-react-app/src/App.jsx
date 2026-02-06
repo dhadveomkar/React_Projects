b16ae51243b2ad934f7305f86f9400dd3a501f2e
dhadveomkar <dhadveomkar08@gmail.com>
2026-02-06 14:56:32 +0530

    My first react app startup

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Simple React Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </div>
+  )
+}
+
+export default App
